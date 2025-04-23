dbc97bbe46330f8e7fe95ee17795784d4b53a4d0
Brian Coca <brian.coca+git@gmail.com>
2025-04-23 10:43:34 -0400

    cleanup + simple timeout

--- plugins/modules/pci_facts.py
+++ plugins/modules/pci_facts.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-
-# (c) 2012, Michael DeHaan <michael.dehaan@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import annotations
@@ -13,18 +11,13 @@ version_added: histerical
 short_description: Gathers PCI facts about remote hosts
 options:
     gather_timeout:
-        version_added: "2.2"
         description:
-            - Set the default timeout in seconds for individual fact gathering.
+            - Set the default timeout in seconds for fact gathering.
         type: int
         default: 10
+        aliases: timeout
 description:
-    - This module is automatically called by playbooks to gather useful
-      variables about remote hosts that can be used in playbooks. It can also be
-      executed directly by C(/usr/bin/ansible) to check what variables are
-      available to a host. Ansible provides many I(facts) about the system,
-      automatically.
-    - This module is also supported for Windows targets.
+    - This module just dumps facts as produced by lscpi command
 extends_documentation_fragment:
   -  action_common_attributes
   -  action_common_attributes.facts
@@ -38,13 +31,14 @@ attributes:
     platform:
         platforms: posix
 author:
-    - "Ansible Core Team"
+    - Brian Coca
 """
 
 EXAMPLES = r"""
 # Display pci facts from all hosts and store them indexed by `hostname` at `/tmp/facts`.
 # ansible all -m pci_facts --tree /tmp/facts
 """
+import signal
 
 from ansible.module_utils.basic import AnsibleModule
 
@@ -56,18 +50,24 @@ def main():
         supports_check_mode=True,
     )
 
-    facts_dict = {}
-    gather_timeout = module.params['gather_timeout']
+    def _timeout(signum, frame):
+        module.exit_json('timeout exceeded while running lscpi')
 
     try:
         lspci = module.get_bin_path('lspci')
     except ValueError as e:
         module.fail_json(f"{e!r}")
 
+    o = signal.signal(signal.SIGALRM, _timeout)
+    signal.alarm(module.params['gather_timeout'])
 
+    # TODO: build in timeout to run_command
     rc, pcidata, err = module.run_command([lspci, '-vvvv', '-D', '-m', '-nn'])
     if rc != 0:
         module.fail_json(f"Error when executing lspci: rc={rc!r} stderr={err!r}")
+
+    signal.alarm(0)
+    signal.signal(signal.SIGALRM, o)
 
     devices = []
     for record in pcidata.split("\n\n"):
