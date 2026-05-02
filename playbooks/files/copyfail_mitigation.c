a447834ecaf720ff92a7bffa19c990f36650bb5d
Brian Coca <brian.coca+git@gmail.com>
2026-05-02 11:43:38 -0400

    Added bpf lsm for 'interception mitigation'
    thanks to Gary Guo for code and instructions

---
+++ playbooks/files/copyfail_mitigation.c
@@ -0,0 +1,41 @@
+// Author: Gary Guo @ RH
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+/* 
+# Requires clang, libbpf, kernel headers and bpf enabled kernel (bpf in /sys/kernel/security/lsm)
+clang -O2 -target bpf -c copyfail_mitigation.bpf.c -o copyfail_mitigation.bpf.o
+
+# Load mitigation
+sudo bpftool prog load copyfail_mitigation.bpf.o /sys/fs/bpf/copyfail_mitigation autoattach
+
+# Unload mitigation
+sudo rm /sys/fs/bpf/copyfail_mitigation
+*/
+
+#include <linux/errno.h>
+#include <linux/if_alg.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#define AF_ALG 38
+
+SEC("lsm/socket_bind")
+int BPF_PROG(block_bind_af_alg, struct socket *sock,
+             struct sockaddr_alg *address, int addrlen) {
+  unsigned short family;
+  if (bpf_probe_read_kernel(&family, sizeof(family), &address->salg_family) != 0 ||
+      family != AF_ALG)
+    return 0;
+
+  char name[__builtin_strlen("authencesn")];
+  struct sockaddr_alg *alg_addr = (struct sockaddr_alg *)address;
+
+  if (bpf_probe_read_kernel(name, sizeof(name), alg_addr->salg_name) != 0 ||
+      __builtin_memcmp(name, "authencesn", 10) != 0)
+    return 0;
+
+  bpf_printk("Blocking bind to AF_ALG authencesn\n");
+  return -ENOENT;
+}
+
+char LICENSE[] SEC("license") = "GPL";
